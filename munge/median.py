0997ae716659dba7f359973c48232885abcbab09
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-06-08 13:31:39 -0400

    clean up

--- munge/median.py
+++ munge/median.py
@@ -22,7 +22,7 @@ def load_data(path, name):
 def rmsle(y, y_pred):
     assert len(y) == len(y_pred)
     terms_to_sum = [(math.log(y_pred[i] + 1) - math.log(y[i] + 1)) ** 2.0 for i, pred in enumerate(y_pred)]
-    return (sum(terms_to_sum) * (1.0/len(y))) ** 0.5
+    return (sum(terms_to_sum) * (1.0 / len(y))) ** 0.5
 
 
 if __name__ == '__main__':
